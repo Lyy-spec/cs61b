8179630bef8f65c67ae47a71c091e87e25579b32
lyq <1325084702@qq.com>
2021-11-16 00:14:57 +0800

    第一次提交

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,11 +1,17 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Lyq
  */
 public class LeapYear {
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
+
+
+    private static boolean isLeapYear(int year){
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    }
+
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
